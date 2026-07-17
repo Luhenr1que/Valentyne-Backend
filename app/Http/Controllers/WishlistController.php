2a76769c7f199c9d77108d57112be5016db405ec
Luhenr1que <lui.hh.araujo@gmail.com>
2026-07-16 23:20:31 -0300

    feat: implement WishlistController with Firestore persistence and external CRM card integration

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -75,9 +75,8 @@ class WishlistController extends Controller
             ])->post('https://gjb2ytrm-p.cagefast.com/api/card', $cardPayload);
 
             if ($cardResponse->successful()) {
-                $cardData = $cardResponse->json();
-                $cardUuid = $cardData['uuid'] ?? ($cardData['data']['uuid'] ?? null);
-                $cardId   = $cardData['id'] ?? ($cardData['data']['id'] ?? null);
+                $cardUuid = $cardData['card']['uuid'] ?? ($cardData['uuid'] ?? ($cardData['data']['uuid'] ?? null));
+                $cardId   = $cardData['card']['id'] ?? ($cardData['id'] ?? ($cardData['data']['id'] ?? null));
 
                 if ($cardUuid && $cardId) {
                     if (!empty($preview['image'])) {
